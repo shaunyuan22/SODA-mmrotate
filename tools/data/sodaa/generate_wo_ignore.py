b8d52b5e88accdbc93d572c4404bb6591c7db9ee
Shaun Yuan <18829043330@163.com>
2023-12-23 16:36:21 +0800

    Add files via upload

---
+++ tools/data/sodaa/generate_wo_ignore.py
@@ -0,0 +1,30 @@
+""" Please enrue this script is in the rawData directory """
+
+import os
+import os.path as osp
+import json
+from tqdm import tqdm
+
+
+cwd = os.getcwd()
+modes = ['train', 'val', 'test']
+for mode in modes:
+    annWithIgnDir = osp.join(cwd, mode, 'AnnsWithIgnore')
+    annDir = osp.join(cwd, mode, 'Annotations')
+    os.mkdir(annDir)
+
+    for file in tqdm(sorted(os.listdir(annWithIgnDir)), ncols=120):
+        annPth = osp.join(annWithIgnDir, file)
+        annFile = json.load(open(annPth, 'r'))
+        annotations = annFile['annotations']
+        anns = []
+        for ann in annotations:
+            if int(ann['category_id']) == 9:
+                continue
+            anns.append(ann)
+        annFile['annotations'] = anns
+        categories = annFile['categories'][:-1]
+        annFile['categories'] = categories
+        desPth = osp.join(annDir, file)
+        json.dump(annFile, open(desPth, 'w'), indent=4)
+
